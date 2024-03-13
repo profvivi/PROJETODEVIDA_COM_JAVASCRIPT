6039c5a293167a087cfe33f831c7499146d1b80d
Viviane <106716101+profvivi@users.noreply.github.com>
2024-03-12 22:41:06 -0300

    s

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const botoes = document.querySelectorAll(".botao");
+/*const botoes = document.querySelectorAll(".botao");
 
 for(let i=0;i <botoes.length;i++){
     botoes[i].onclick = function(){
@@ -8,5 +8,21 @@ for(let i=0;i <botoes.length;i++){
         }
 
         botoes[i].classList.add("ativo");
+    }
+}*/
+
+const botoes = document.querySelectorAll(".botao");//aula3
+const textos = document.querySelectorAll(".aba-conteudo");//aula5
+
+for (let i = 0; i < botoes.length; i++) { //aula3
+    botoes[i].onclick = function () { //aula3
+
+        for (let j = 0; j < botoes.length; j++) {
+            botoes[j].classList.remove("ativo");//aula5
+            textos[j].classList.remove("ativo");//aula5
+        } //aula3
+
+        botoes[i].classList.add("ativo");//aula5
+        textos[i].classList.add("ativo");//aula5
     }
 }
