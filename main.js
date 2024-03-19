9c0af050ed199cc74fbb451a730b35a70786cb75
Viviane <106716101+profvivi@users.noreply.github.com>
2024-03-19 19:54:41 -0300

    s

--- main.js
+++ main.js
@@ -42,7 +42,7 @@ for (let i = 0; i < botoes.length; i++) {
         textos[i].classList.add("ativo");
     }
 }
-
+/*
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
 let tempoAtual = new Date();
