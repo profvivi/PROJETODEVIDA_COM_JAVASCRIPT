06358a9d8736265df6fdd1bb820004c01a80adca
Viviane <106716101+profvivi@users.noreply.github.com>
2024-04-23 20:12:06 -0300

    s

--- main.js
+++ main.js
@@ -123,7 +123,7 @@ function calculaTempo(tempoObjetivo) {
 Fim da 2ª explicação */
 
 //3ª explicação - função para atualizar o cronômetro:
-
+/*
 // definição do tempo dos objetivos
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2024-10-05T00:00:00");
@@ -176,3 +176,59 @@ function calculaTempo(tempoObjetivo) {
 que seja exibida uma mensagem de que o prazo foi esgotado. Dessa
 forma, a contagem do tempo que resta ocorrerá apenas se esse tempo
 for positivo.*/
+*/
+
+//Aula7 - Automatizando atualizações (código aula6 abaixo)
+const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".aba-conteudo");
+
+for (let i = 0; i < botoes.length; i++) {
+    botoes[i].onclick = function () {
+
+        for (let j = 0; j < botoes.length; j++) {
+            botoes[j].classList.remove("ativo");
+            textos[j].classList.remove("ativo");
+        }
+
+        botoes[i].classList.add("ativo");
+        textos[i].classList.add("ativo");
+    }
+}
+
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2024-03-05T00:00:00");
+const tempoObjetivo2 = new Date("2024-10-30T00:00:00"); //adicionado
+const tempoObjetivo3 = new Date("2024-11-05T00:00:00"); //adicionado
+const tempoObjetivo4 = new Date("2024-12-30T00:00:00"); //adicionado
+
+const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];//adicionado
+
+function calculaTempo(tempoObjetivo) {
+    let tempoAtual = new Date();
+    let tempoFinal = tempoObjetivo - tempoAtual;
+    let segundos = Math.floor(tempoFinal / 1000);
+    let minutos = Math.floor(segundos / 60);
+    let horas = Math.floor(minutos / 60);
+    let dias = Math.floor(horas / 24);
+
+    segundos %= 60;
+    minutos %= 60;
+    horas %= 24;
+ 
+    if (tempoFinal > 0){
+        return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos"; //adicionado dentro do if
+    } else {
+        return "Prazo Finalizado";
+    }
+}
+
+function atualizaCronometro(){ //adicionado
+    for (let i=0; i<contadores.length;i++){ //adicionado
+        contadores[i].textContent = calculaTempo(tempos[i]);  //adicionado
+    }
+}
+
+function comecaCronometro(){ //adicionado
+    atualizaCronometro(); //adicionado
+    setInterval(atualizaCronometro,1000); //adicionado
+}
