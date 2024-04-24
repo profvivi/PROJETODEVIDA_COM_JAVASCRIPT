10f6d8665389aac12a11b313ba74a2f2270a19b9
Viviane <106716101+profvivi@users.noreply.github.com>
2024-04-24 01:46:27 -0300

    s

--- main.js
+++ main.js
@@ -95,9 +95,11 @@ function calculaTempo(tempoObjetivo) {
     if (tempoFinal > 0){
         return [dias,horas,minutos,segundos];
     } else {
-        return [0,0,0,0];
         return "Prazo Finalizado"
     }
+    else {
+    return [0,0,0,0];
+}
 }
 
 function atualizaCronometro(){
