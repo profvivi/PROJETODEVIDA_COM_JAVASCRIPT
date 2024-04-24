1f72f9a43af26926f28fd58842e4342a179f9782
Viviane <106716101+profvivi@users.noreply.github.com>
2024-04-24 08:17:58 -0300

    s

--- main.js
+++ main.js
@@ -93,7 +93,7 @@ function calculaTempo(tempoObjetivo) {
     minutos %= 60;
     horas %= 24;
     if (tempoFinal > 0){
-        return [dias,horas,minutos,segundos];
+        return ["" + dias,horas,minutos,segundos];
     } else {
         return ["Prazo Finalizado" + 0,0,0,0];
     }
